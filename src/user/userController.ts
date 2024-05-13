371419b8d5a7fa248a0a6e5a2f99112066c71fac
Harsh sahu <sahuharsh164@gmail.com>
2024-05-13 16:18:55 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -51,12 +51,39 @@ try {
     //process
 
     //response
-    res.json({accessToken: token});
+    res.status(201).json({accessToken: token});
 } catch (error) {
     return next(createHttpError(500, "Error while signing the jwt token"))
 }
 
 }
 
+const loginUser=async(req:Request, res:Response, next:NextFunction)=>{
 
-export {createUser};
+    const {email, password}=req.body;
+
+    if(!email || !password){
+        return next(createHttpError(400,"All fields Required"))
+    }
+    const user=await userModel.findOne({email});
+
+    if(!user){
+        return next(createHttpError(404,"User not found."));
+    }
+    const isMatch=await bcrypt.compare(password, user.password)
+
+    if(!isMatch){
+        return next(createHttpError(401, "Username or password incorrect!"))
+    }
+    //create Accestoken\
+    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+        expiresIn: "7d",
+        algorithm: "HS256",
+      });
+    
+
+    res.json({accessToken:token})
+}
+
+
+export {createUser,loginUser};
